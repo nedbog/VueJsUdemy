1211bd9fb1e843f8eddc63fcfbfc3a1bd8053956
Bogdan Bog <nedbog@gmail.com>
2020-10-10 17:08:38 +0300

    first commit

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <server-status></server-status>
+  </div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    serverStatus: ServerStatus
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ client/src/components/ServerStatus.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Status: {{ status }}</h1>
+    <hr />
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      status: "Critical",
+    };
+  },
+  methods: {
+    changeStatus: function () {
+      this.status = "Normal";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
